3609e04bbff87fcc8b11ebd66d612703fe047a3b
NMoore42 <nicholas.moore5@icloud.com>
2019-05-07 12:02:59 -0500

    addded controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #As you work through, please modify your resources to match only thoses needed:
+
+  resources :follows
+  resources :sections
+  resources :stories
+  resources :submissions
+  resources :users
+  resources :votes
+  resources :sessions
 end
