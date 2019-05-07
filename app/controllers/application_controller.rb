0e9ed128cbfdc75a9d91048f3a6a36f07e482444
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-07 15:20:01 -0500

    created session methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user_id
+
+  def current_user_id
+    session[:user_id]
+  end
+
+  def require_login
+    # If session does not include any value for :current_user key
+    # Redirect this user back to the stories index or root page.
+    if !current_user_id
+      redirect_to controller: 'stories', action 'index'
+    end
+  end
+
 end
