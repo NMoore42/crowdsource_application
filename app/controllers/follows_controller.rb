794b43e28c4302de3a962948d1961957139b8281
NMoore42 <nicholas.moore5@icloud.com>
2019-05-08 10:20:11 -0500

    added follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,22 @@
 class FollowsController < ApplicationController
+
+  def new
+    @follow = Follow.new
+  end
+
+  def create
+    @follow = Follow.new(follow_params)
+    if @follow.save
+      #do something
+    else
+      redirect_to login_path
+    end
+  end
+
+  private
+
+  def follow_params
+    params.require(:follow).permit(:user_id, :story_id)
+  end
+
 end
