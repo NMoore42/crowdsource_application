4d80f5af9a1a99c62d28dcf0fe9d5695f50952db
NMoore42 <nicholas.moore5@icloud.com>
2019-05-08 17:44:34 -0500

    creates new section when submission becomes cannon

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -14,6 +14,7 @@ class Vote < ApplicationRecord
   def cannonize_submission
     self.submission.update(winner?: true)
     self.section.update(published?: true)
+    Section.create(story_id: self.story.id, published?: false)
   end
 
 
