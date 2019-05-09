997a5246e8373d82413135a4b0af6307a4d1edbb
NMoore42 <nicholas.moore5@icloud.com>
2019-05-09 13:46:22 -0500

    minor changes

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,11 +4,7 @@ class Section < ApplicationRecord
   has_many :authors, through: :submissions
 
   def order_submissions
-    self.submissions.sort_by {|submission| submission.votes.count}
-  end
-
-  def get_published_submission
-    Submission.all.find_by {|submission| submission.winner? && submission.section == self}
+    self.submissions.sort_by {|submission| -submission.votes.count}
   end
 
 
